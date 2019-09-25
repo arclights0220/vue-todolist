9e3bc9656f7572ee8826ba275ed0b663ece8e680
mendox0220 <mendox0220@gmail.com>
2019-09-25 23:56:52 +0900

    todolist

---
+++ src/components/ClubList.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+     <div class="club" :class="{'club-check':club.check}" v-for="(club,idx) in clublist" :key="club.name">
+      <h1> {{club.name}} </h1>
+      <p>컴퓨터실 : {{club.computer}} </p>
+      <button @click="deleteThis(idx)">삭제</button>
+      <button @click="checkThis(idx)">확인</button>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    props:{
+        clublist: Array
+    },
+    methods:{
+        deleteThis(index){
+            this.clublist.splice(index,1);
+        },
+        checkThis(index){
+            this.clublist[index].check = true;
+        }
+    }
+}
+</script>
+
+<style scoped>
+.club{
+    background-color: beige;
+}
+.club-check{
+    color: #aaefde;
+}
+</style>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="home">
+    <!-- <div v-if="hello">
+      a
+    </div>
+    <div v-else>
+      b
+    </div>-->
+    <!-- input받는거 실시간으로 -->
+    <!-- <input type="text" v-model="input">
+    {{input}}-->
+    <!-- key는 같으면 안됨
+    <div v-for="item in arr" :key="item">
+      {{item}}
+    </div>-->
+    <!-- 실시간으로 변하는 것
+    <div v-if="toggle">HEllo</div>
+    <div v-else>World</div>
+    <button @click="toggleFunction">Click</button>--> 
+   
+    <div>
+      <input type="text" v-model="curName" />
+      <br />
+      <input type="number" v-model="curCom" />
+      <button @click="addClub">추가하기</button>
+    </div>
+     <ClubList :clublist="data" />
+  </div>
+</template>
+ 
+<script>
+import ClubList from "../components/ClubList.vue";
+export default {
+  //data라는 함수에 return값으로 변수 선언
+  components: {
+    ClubList
+  },
+  methods: {
+    addClub(){
+      this.data.push({
+        name : this.curName,
+        computer: this.curCom
+      })
+    }
+  },
+  data() {
+    return {
+      curName: "",
+      curCom: 0,
+      // hello:1,
+
+      // arr:["I","W","O","P"],
+
+      // toggle: false
+      data: [
+        {
+          name: "EDCAN",
+          computer: 16,
+          check:false
+        },
+        {
+          name: "IWOP",
+          computer: 9,
+          check:false
+        }
+      ]
+    };
+  }
+  // methods: {
+  //   toggleFunction() {
+  //     this.toggle = !this.toggle;
+  //   }
+  // }
+};
+</script>
+
+<style>
+.home {
+  
+}
+</style>
